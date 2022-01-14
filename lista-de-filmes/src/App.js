eb16d48afa90c36db000ff17d2f047a2638fc71d
Talison <talisonsantana76@gmail.com>
2022-01-14 15:16:57 -0300

    inserindo componente filmes, e colocando as props que serao chamadas pelo componente

--- lista-de-filmes/src/App.js
+++ lista-de-filmes/src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>
+        Filmes favoritos
+      </h1>
     </div>
   );
 }
---
+++ lista-de-filmes/src/components/Filmes.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './Filmes.css';
+import { Link } from 'react-router-dom'
+
+
+
+class Filmes extends Component {
+
+  render() {
+    const { title, poster_path } = this.props;
+
+    return (
+      <nav>
+        <ul className="container" >
+          <li >
+            <Link to={title}>
+              <h3>{title}</h3>
+              <img src={poster_path} alt={title} />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Filmes;
